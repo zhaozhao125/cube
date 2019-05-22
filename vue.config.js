1dcd3966dcf9eb79396ee36fbc24eb4d14c07cef
zhaojinpeng <1406737142@qq.com>
2019-05-22 18:48:40 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -23,19 +23,19 @@ module.exports = {
   },
   devServer: {
     before(app) {
-      app.get('app/seller', function (req, res) {
+      app.get('/api/seller', function (req, res) {
         res.json({
           errno: 0,
           data: seller
         })
       })
-      app.get('app/goods', function (req, res) {
+      app.get('/api/goods', function (req, res) {
         res.json({
           errno: 0,
           data: goods
         })
       })
-      app.get('app/ratings', function (req, res) {
+      app.get('/api/ratings', function (req, res) {
         res.json({
           errno: 0,
           data: ratings
