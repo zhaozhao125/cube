bf5fe3d1ef1d20c1af72133551e922b97450e70c
zhaojinpeng <1406737142@qq.com>
2019-06-04 16:47:46 +0800

    icon

--- src/components/support/support.vue
+++ src/components/support/support.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>1111</div>
+  <span class="support" :class="setClass">
+    <!-- <img src="./brand@3x.png" alt=""> -->
+  </span>
 </template>
 <script>
 export default {
-
+  props: {
+    name: {
+      default: '',
+      type: String
+    },
+    size: {
+      default: '',
+      type: String
+    }
+  },
+  computed: {
+    setClass() {
+      console.log(`${this.name} ${this.size}`)
+      return `${this.name} ${this.size}`
+    }
+  }
 }
 </script>
-<style lang="stylus" scoped>
-   @import "~common/stylus/mixin"
+<style lang="stylus">
+  @import "~common/stylus/mixin"
   //  @import "~common/stylus/mixin"
+  .support
+    display: inline-block;
+    &.big
+      width:32px;
+      height:17px;
+      background-size: 32px 17px;
+    &.small
+      width:15px;
+      height:12px;
+      background-size: 15px 12px;
+    &.brand
+      bg-image('brand')
+      background-repeat: no-repeat;
+    &.discount_1
+      bg-image('discount_1')
+      background-repeat: no-repeat;
 </style>
  
--- src/components/v-header/v-header.vue
+++ src/components/v-header/v-header.vue
@@ -4,13 +4,23 @@
       <div class="top-content">
         <img src="http://static.galileo.xiaojukeji.com/static/tms/seller_avatar_256px.jpg" alt="">
         <div class="des">
-          <div class="fz-16">粥品香坊（回龙观）</div>
-          <div class="fz-12">
-            蜂鸟专送/38分钟送达
+          <div class="fz-16">
+            <support size="big" name="brand"/>
+            <span class="m-5">粥品香坊（回龙观）</span>
           </div>
-          <div class="fz-12">在线支付满28减5</div>
+          <div class="fz-12">
+            <span>蜂鸟专送/38分钟送达</span>
+          </div>
+          <div class="fz-12">
+            <support size="small" name="discount_1"/>
+            <span>在线支付满28减5</span>
+            </div>
         </div>
-        <div class="count">11</div>
+        <div class="count">
+          <div class="five">5个
+            <div class="arrow"></div>
+          </div>
+        </div>
       </div>
       <div class="bgc">
         <img src="http://static.galileo.xiaojukeji.com/static/tms/seller_avatar_256px.jpg" alt="">
@@ -20,10 +30,13 @@
   </div>
 </template>
 <script>
-
+import support from '../support/support'
 export default {
   data () {
     return {}
+  },
+  components: {
+    support
   }
 }
 </script>
@@ -31,6 +44,8 @@ export default {
   @import "~common/stylus/mixin"
   .header
     .header-top
+      .m-5
+        margin-left:5px;
       position: relative;
       background: rgba(7,17,27,0.5);
       padding: 20px;
@@ -41,7 +56,10 @@ export default {
         width:100%;
         height:100%;
         z-index: -1;
-
+        img
+          width:100%;
+          height:100%;
+          filter: blur(10px)
       .top-content
         display flex;
         img
@@ -52,11 +70,40 @@ export default {
           padding-left:5px;
           flex:1;
           .fz-16
+            display: flex;
+            align-items: center;
             font-size 16px;
             line-height 16px;
             margin-bottom:10px;
           .fz-12
+            display: flex;
+            align-items: center;
             font-size 12px;
             line-height 12px;
             margin-bottom:10px;
+        .count
+          display flex;
+          align-items:flex-end;
+          padding-bottom:8px;
+          .five
+            background: rgba(0,0,0,0.2);
+            width:45px;
+            height:20px;
+            border-radius: 10px;
+            line-height: 20px;
+            color: #fff;
+            font-size: 12px;
+            padding-left: 10px;
+            box-sizing: border-box;
+            position: relative
+           .arrow
+             position: absolute;
+             width:5px;
+             height:5px;
+             border: 1px solid #fff;
+             transform: rotate(45deg)
+             border-left: none;
+             border-bottom: none;
+             top: 6px;
+             right: 8px;
 </style>
